68b1f16a67be72fb3da993ed02d2f4370a9713c3
Julian Camacho <jdacamacho@unicauca.edu.co>
2025-12-28 22:19:13 -0500

    Added docker configuration

--- src/server.ts
+++ src/server.ts
@@ -49,10 +49,10 @@ app.use((req, res, next) => {
 
 /**
  * Start the server if this module is the main entry point.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
+ * The server listens on the port defined by the `PORT` environment variable, or defaults to 4200.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'] || 4200;
   app.listen(port, (error) => {
     if (error) {
       throw error;
